7f297b738c137c39c90189d86a65410105d307fb
Dennis Kamau <devjam.ke@gmail.com>
2022-09-15 19:41:57 +0300

    Generate predefined data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Prescription.destroy_all
+Prescription.create!([{
+    vet_prescription:"Achyranthes aspera",
+    prescribed_by:"Dr Chucks",
+    disease:"Rabbies",
+    livestock:"goat",
+    completed:false
+},
+{
+    vet_prescription:"Achyranthes aspera",
+    prescribed_by:"Dr Rex",
+    disease:"Rabbies",
+    livestock:"Dog",
+    completed:false
+},
+{
+    vet_prescription:"ethnovet",
+    prescribed_by:"Dr Chucks",
+    disease:"Pox",
+    livestock:"Sheep",
+    completed:false
+}])
+p "Created #{Prescription.count} prescriptions"
